128838fde360391f02ff0b11d1d8df296daf6077
Ubuntu <ubuntu@ip-172-31-17-240.us-east-2.compute.internal>
2019-10-18 20:00:48 +0000

    q2-3

--- js-intermediate.js
+++ js-intermediate.js
@@ -7,12 +7,18 @@
 
 
 
-// 2. Write a function called oddChecker that takes in an array and returns a new array of only odd numbers.
 
-var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-// Expected output: [9, 7, 199, -9]
-var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
-// Expected output: [7, 23, -823]
+// // 2. Write a function called oddChecker that takes in an array and returns a new array of only odd numbers.
+
+// var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+// // Expected output: [9, 7, 199, -9]
+// var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+// // Expected output: [7, 23, -823]
+
+// const oddChecker = (array) => array.filter(element => typeof element === 'number' && element % 2 !== 0) 
+
+// console.log(oddChecker(fullArr1))
+// console.log(oddChecker(fullArr2))
 
 
 
@@ -29,77 +35,77 @@ var bicycle = {
 }
 
 // Log the type of bicycle:
-console.log()
+console.log(bicycle.type)
 
 // Log the bell:
-console.log()
+console.log(bicycle.gear[2])
 
 // Log the PSI:
 console.log()
 
 
-// 4. Write a function that takes in an array and returns an array in reverse order.
+// // 4. Write a function that takes in an array and returns an array in reverse order.
 
-var originalArray1 = [1, 2, 3, 4, 5, 6, 7]
-// Expected output: [7, 6, 5, 4, 3, 2, 1],
-var originalArray2 = ["9", "1", "o", "h", "c", "e"]
-// Expected output: ["e", "c", "h", "o", "1", "9"]
-
-
-
-// 5. Write the code that would make this test pass.
-
-describe("multTwo", () => {
-    test ("returns an array with all the numbers multiplied by two", () => {
-        expect(multTwo([3, 4, 5])).toEqual([6, 8, 10])
-        expect(multTwo([23, -9, 0])).toEqual([46, -18, 0])
-        expect(multTwo([4.5, -4.5, 12])).toEqual([9, -9, 24])
-    })
-})
+// var originalArray1 = [1, 2, 3, 4, 5, 6, 7]
+// // Expected output: [7, 6, 5, 4, 3, 2, 1],
+// var originalArray2 = ["9", "1", "o", "h", "c", "e"]
+// // Expected output: ["e", "c", "h", "o", "1", "9"]
 
 
 
-// 6. Write a function called letterCounter that logs the number of times the letter "l" appears in ourString.
+// // 5. Write the code that would make this test pass.
 
-var ourString = "Hello Learn Students!"
-// Expected output: 3
-
-
-
-// 7. Create a function that takes in a string of a single word and returns the middle letter of the word. If the word is an even number of letters, return the two middle letters.
-
-var middleLetters1 = "hello"
-// Expected output: “l”
-var middleLetters2 = "rhinoceros"
-// Expected output: “oc”
+// describe("multTwo", () => {
+//     test ("returns an array with all the numbers multiplied by two", () => {
+//         expect(multTwo([3, 4, 5])).toEqual([6, 8, 10])
+//         expect(multTwo([23, -9, 0])).toEqual([46, -18, 0])
+//         expect(multTwo([4.5, -4.5, 12])).toEqual([9, -9, 24])
+//     })
+// })
 
 
 
+// // 6. Write a function called letterCounter that logs the number of times the letter "l" appears in ourString.
 
-// 8. Write a program to get the area of a sphere using object classes. Create three spheres with different radi as test cases. Area of a sphere =  4πr^2 (four pi r squared)
+// var ourString = "Hello Learn Students!"
+// // Expected output: 3
+
+
+
+// // 7. Create a function that takes in a string of a single word and returns the middle letter of the word. If the word is an even number of letters, return the two middle letters.
+
+// var middleLetters1 = "hello"
+// // Expected output: “l”
+// var middleLetters2 = "rhinoceros"
+// // Expected output: “oc”
 
 
 
 
-// 9. Use object destructuring to return the nested values of specs. Expected output: 4, "manual"
-// Don't overthink this one, it's just destrcturing practice.
-
-var myCar = {
-  make: "VW",
-  model: "Jetta",
-  specs: {
-    doors: 4,
-    transmission: "manual",
-  }
-}
+// // 8. Write a program to get the area of a sphere using object classes. Create three spheres with different radi as test cases. Area of a sphere =  4πr^2 (four pi r squared)
 
 
 
-// 10. STRETCH: Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
-var numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
-var numbersToAdd2 = [0, 7, -8, 12]
-// Expected output: [0, 7, -1, 11]
-var numbersToAdd3 = []
-// Expected output: []
+// // 9. Use object destructuring to return the nested values of specs. Expected output: 4, "manual"
+// // Don't overthink this one, it's just destrcturing practice.
+
+// var myCar = {
+//   make: "VW",
+//   model: "Jetta",
+//   specs: {
+//     doors: 4,
+//     transmission: "manual",
+//   }
+// }
+
+
+
+// // 10. STRETCH: Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+
+// var numbersToAdd1 = [2, 4, 45, 9]
+// // Excpected output: [2, 6, 51, 60]
+// var numbersToAdd2 = [0, 7, -8, 12]
+// // Expected output: [0, 7, -1, 11]
+// var numbersToAdd3 = []
+// // Expected output: []
